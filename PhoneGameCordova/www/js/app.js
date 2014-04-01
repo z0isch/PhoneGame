a86df8ee45b1ab6b58f2debbdfe61dc8d4e9d241
AJ <aruf@bellarmine.edu>
2014-04-01 15:17:59 -0400

    made the oauthpage a little easier to test with

--- PhoneGameCordova/www/js/app.js
+++ PhoneGameCordova/www/js/app.js
@@ -10,5 +10,17 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    var authWindow = window.open("https://accounts.google.com/o/oauth2/auth?redirect_uri=http%3a%2f%2flocalhost%3a55961%2fhome%2fGoogleOAuth&response_type=code&client_id=553327898639-2ngq3i7eve550c7et33b2789i9fseh67.apps.googleusercontent.com&scope=profile",
+         "_blank", 'location=no,toolbar=no');
+    authWindow.addEventListener('loadstart', function (e) {
+      var url = e.url;
+      var code = /\?code=(.+)$/.exec(url);
+      var error = /\?error=(.+)$/.exec(url);
+      if (code || error) {
+        authWindow.close();
+        alert(code[1]);
+      }
+      
+    });
   });
 })
--- PhoneGameWebApi/Controllers/HomeController.cs
+++ PhoneGameWebApi/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Mvc;
 using log4net;
 using PhoneGameService;
+using PhoneGameService.Models.OAuthProviders;
+using PhoneGameService.Services;
 
 namespace PhoneGameWebApi.Controllers
 {
@@ -21,6 +23,7 @@ namespace PhoneGameWebApi.Controllers
 
         public ActionResult GoogleOAuth(string code)
         {
+            ViewBag.GoogleUrl = OAuthService.GetOAuthUrl(new Google());
             ViewBag.Code = code;    
             return View();
         }
