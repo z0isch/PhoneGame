adecaf12331934674cebbbce846135ec9d686fb6
AJ <aruf@bellarmine.edu>
2014-04-01 18:01:10 -0400

    Made some buttons on the cordova front end

--- PhoneGameCordova/www/js/app.js
+++ PhoneGameCordova/www/js/app.js
@@ -3,24 +3,31 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
+angular.module('PhoneGame', ['ionic'])
+.controller('LoginCtrl', function ($scope, $ionicPlatform) {
+  $scope.loggedIn = false;
+  $scope.status='<h3>You are not logged in!</h3><p>Please use an OAuth provider below:</p>';
+  $ionicPlatform.ready(function () {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
-    var authWindow = window.open("https://accounts.google.com/o/oauth2/auth?redirect_uri=http%3a%2f%2flocalhost%3a55961%2fhome%2fGoogleOAuth&response_type=code&client_id=553327898639-2ngq3i7eve550c7et33b2789i9fseh67.apps.googleusercontent.com&scope=profile",
-         "_blank", 'location=no,toolbar=no');
-    authWindow.addEventListener('loadstart', function (e) {
-      var url = e.url;
-      var code = /\?code=(.+)$/.exec(url);
-      var error = /\?error=(.+)$/.exec(url);
-      if (code || error) {
-        authWindow.close();
-        alert(code[1]);
-      }
-      
-    });
+    $scope.googleLogin = function () {
+      var authWindow = window.open("https://accounts.google.com/o/oauth2/auth?redirect_uri=http%3a%2f%2flocalhost%3a55961%2fhome%2fGoogleOAuth&response_type=code&client_id=553327898639-2ngq3i7eve550c7et33b2789i9fseh67.apps.googleusercontent.com&scope=profile",
+           "_blank", 'location=no,toolbar=no');
+      authWindow.addEventListener('loadstart', function (e) {
+        var url = e.url;
+        var code = /\?code=(.+)$/.exec(url);
+        var error = /\?error=(.+)$/.exec(url);
+
+        if (code || error) {
+          authWindow.close();
+          $scope.status = '<h3>You have gotten a code from Google!</h3>';
+          $scope.code = code[1];
+          $scope.loggedIn = true;
+          $scope.$apply();
+        }
+
+      });
+    }
   });
-})
+});
